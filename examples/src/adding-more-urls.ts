0a29ee3569cfdda933c621db1eb42c681173f71e
Takahito Urai <uraitakahito@gmail.com>
2024-09-27 23:33:29 +0000

    Add examples

---
+++ examples/src/adding-more-urls.ts
@@ -0,0 +1,11 @@
+import { CheerioCrawler } from 'crawlee';
+
+const crawler = new CheerioCrawler({
+    async requestHandler({ $, request }) {
+        const title = $('title').text();
+        console.log(`The title of "${request.url}" is: ${title}.`);
+    }
+})
+
+await crawler.run(['https://crawlee.dev']);
+
