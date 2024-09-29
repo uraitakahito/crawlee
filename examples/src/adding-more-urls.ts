828ccc10d2924d6c7622fc7d1dcfd45714a4fd65
Takahito Urai <uraitakahito@gmail.com>
2024-09-29 09:44:56 +0000

    yarn run format

--- examples/src/adding-more-urls.ts
+++ examples/src/adding-more-urls.ts
@@ -4,16 +4,16 @@ const crawler = new CheerioCrawler({
     async requestHandler({ $, request }) {
         const title = $('title').text();
         console.log(`The title of "${request.url}" is: ${title}.`);
-    }
-})
+    },
+});
 
 async function main() {
-  try {
-    const res = await crawler.run(['https://crawlee.dev']);
-    console.log("Main function response:", res);
-  } catch (error) {
-    console.error("Error in main function response:", error);
-  }
+    try {
+        const res = await crawler.run(['https://crawlee.dev']);
+        console.log('Main function response:', res);
+    } catch (error) {
+        console.error('Error in main function response:', error);
+    }
 }
 
 main();
